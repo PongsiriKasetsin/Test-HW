7e6b4194ca73446c9e62a4af98c672b8684bd15c
Pongsiri.kasetsin <Pongsiri.kase@bumail.net>
2017-04-22 15:35:30 +0700

    5.สร้างกระสุนให้เคลื่อนที่

--- Space Shooter/Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Space Shooter/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+	[System.Serializable]
+	public class Boundary 
+	{
+		public float xMin, xMax, zMin, zMax;
+	}
+		
 	public float speed;
+	public float tilt;
+	public Boundary boundary;
 
 	void FixedUpdate ()
 	{
@@ -13,5 +22,13 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
 }
