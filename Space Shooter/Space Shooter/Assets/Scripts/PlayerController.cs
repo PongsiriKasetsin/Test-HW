edf8e30702a128f86e2a10f381d1ffb6b63f7ece
Pongsiri.kasetsin <Pongsiri.kase@bumail.net>
2017-04-22 15:51:46 +0700

    6.ยิงกระสุนออกไปเลื่อยๆ

--- Space Shooter/Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Space Shooter/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,26 @@ public class PlayerController : MonoBehaviour {
 	{
 		public float xMin, xMax, zMin, zMax;
 	}
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
 		
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
 
 	void FixedUpdate ()
 	{
